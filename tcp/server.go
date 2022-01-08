5adece5df4a0e0ce67938797a5c113ee015b8968
Matrix <matrix32768@163.com>
2022-01-08 12:13:12 +0800

    add ABNF

--- tcp/server.go
+++ tcp/server.go
@@ -1,5 +1,22 @@
 package tcp
 
+/*
+缓存的 tcp 服务实现，服务端与客户端采用 ABNF 协议通信
+
+command = op key | key-value
+op = 'S' | 'G' | 'D'
+key = bytes-array
+bytes-array = lenght SP content
+length = 1 * DIGIT
+content = *OCTET
+key-value = length SP length SP content content
+response = error | bytes-array
+error = '-' bytes-array
+
+DIGIT 取值 0~9
+OCTET 取值 0x00~0xFF
+*/
+
 import (
 	"net"
 
